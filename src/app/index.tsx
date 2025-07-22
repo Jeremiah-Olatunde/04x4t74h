1131f92edd01c749232ab96100db6d83476b547f
Jesuseun Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2025-07-23 00:10:06 +0100

    chore(deps setup): add package prettier prettier-plugin-tailwindcss

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,12 @@
 export function App() {
   return (
-    <section className="p-6 flex flex-col gap-4">
+    <section className="flex flex-col gap-4 p-6">
       <header className="flex flex-col gap-2">
-        <h1 className="font-fredoka text-4xl font-medium text-primary">
+        <h1 className="font-fredoka text-primary text-4xl font-medium">
           The Second Coming
         </h1>
         <div className="flex">
-          <h2 className="font-sora text-sm font-semibold text-white rounded-md bg-secondary p-1">
+          <h2 className="font-sora bg-secondary rounded-md p-1 text-sm font-semibold text-white">
             William Butler Yeats
           </h2>
         </div>
@@ -18,5 +18,5 @@ export function App() {
         ceremony of innocence is drown
       </p>
     </section>
-  );
+  )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,19 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
 
-import * as Dom from "@/lib/fp-ts/DOM.ts";
-import * as Function from "@/lib/fp-ts/Function";
-import * as IOOption from "@/lib/fp-ts/IOOption";
+import * as Dom from "@/lib/fp-ts/DOM.ts"
+import * as Function from "@/lib/fp-ts/Function"
+import * as IOOption from "@/lib/fp-ts/IOOption"
 
-import "./index.css";
-import { App as App_ } from "./app";
+import "./index.css"
+import { App as App_ } from "./app"
 
 export function App() {
   return (
     <StrictMode>
       <App_ />
     </StrictMode>
-  );
+  )
 }
 
 Function.pipe(
@@ -22,4 +22,4 @@ Function.pipe(
   IOOption.map(createRoot),
   IOOption.map((root) => root.render(<App />)),
   IOOption.unsafeExpect("Element not found: [#root]"),
-);
+)
