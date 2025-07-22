ac65e1376f578562765521e52fc0f0a2e3b7a2c9
Jesuseun Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2025-07-22 23:51:26 +0100

    style: add color primary secondary

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,12 +2,14 @@ export function App() {
   return (
     <section className="p-6 flex flex-col gap-4">
       <header className="flex flex-col gap-2">
-        <h1 className="font-fredoka text-4xl font-semibold text-stone-700">
+        <h1 className="font-fredoka text-4xl font-medium text-primary">
           The Second Coming
         </h1>
-        <h2 className="font-sora text-xl font-medium text-stone-600">
-          William Butler Yeats
-        </h2>
+        <div className="flex">
+          <h2 className="font-sora text-sm font-semibold text-white rounded-md bg-secondary p-1">
+            William Butler Yeats
+          </h2>
+        </div>
       </header>
       <p className="font-sora text-stone-600">
         Turning and turning in the widening gyre the falcon cannot hear the
