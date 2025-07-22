7830b54c36cdf14ff402bb8c5fc89dd5adc0a184
Jesuseun Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2025-07-22 23:03:41 +0100

    chore(setup): tailwind

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,13 @@
 export function App() {
   return (
-    <section>
-      <header>
-        <h1>The Second Coming</h1>
-        <h2>William Butler Yeats</h2>
+    <section className="p-6 flex flex-col gap-4">
+      <header className="flex flex-col gap-2">
+        <h1 className="text-2xl font-bold text-stone-700">The Second Coming</h1>
+        <h2 className="text-xl font-semibold text-stone-600">
+          William Butler Yeats
+        </h2>
       </header>
-      <p>
+      <p className="text-stone-600">
         Turning and turning in the widening gyre the falcon cannot hear the
         falconer things fall apart, the center cannot hold mere anarchy is
         loosed upon the land the blood dimmed tide is loosed and everywhere the
