9571a6b4024b7089e934d70fcdb9099045c041ad
Jesuseun Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2025-07-23 15:51:23 +0100

    fix(ui): component Badge

--- src/components/badge/index.tsx
+++ src/components/badge/index.tsx
@@ -10,7 +10,7 @@ export function Badge({
       className={`${variant === "primary" ? "bg-primary/10" : variant === "secondary" ? "bg-secondary/10" : variant === "tertiary" ? "bg-neutral-100" : null} flex items-center justify-center rounded-s-full rounded-e-full px-3 py-1`}
     >
       <span
-        className={`text-xs ${variant === "primary" ? "text-primary" : variant === "secondary" ? "text-secondary" : variant === "tertiary" ? "text-neutral-400" : null} font-sora font-medium`}
+        className={`text-xxs ${variant === "primary" ? "text-primary" : variant === "secondary" ? "text-secondary" : variant === "tertiary" ? "text-neutral-400" : null} font-sora font-medium`}
       >
         {text}
       </span>
