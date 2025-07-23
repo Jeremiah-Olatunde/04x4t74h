7a59315f7c2a691372571c4dfa7c7643ec3e3954
Jesuseun Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2025-07-23 16:25:40 +0100

    feat(ui): component Badge

--- src/components/badge/index.tsx
+++ src/components/badge/index.tsx
@@ -7,10 +7,10 @@ export function Badge({
 }) {
   return (
     <div
-      className={`${variant === "primary" ? "bg-primary/10" : variant === "secondary" ? "bg-secondary/10" : variant === "tertiary" ? "bg-neutral-100" : null} flex items-center justify-center rounded-s-full rounded-e-full px-3 py-1`}
+      className={`${variant === "primary" ? "bg-primary/10" : variant === "secondary" ? "bg-secondary" : variant === "tertiary" ? "bg-neutral-100" : null} flex items-center justify-center rounded-s-full rounded-e-full px-3 py-1`}
     >
       <span
-        className={`text-xxs ${variant === "primary" ? "text-primary" : variant === "secondary" ? "text-secondary" : variant === "tertiary" ? "text-neutral-400" : null} font-sora font-medium`}
+        className={`text-xxs ${variant === "primary" ? "text-primary" : variant === "secondary" ? "text-neutral-600" : variant === "tertiary" ? "text-neutral-600" : null} font-sora font-medium`}
       >
         {text}
       </span>
