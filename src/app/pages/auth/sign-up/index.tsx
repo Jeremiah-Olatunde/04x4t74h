318ed9dff8fa48b7e2811d04dd8414147f2b62bf
Jesuseun Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2025-07-25 04:07:03 +0100

    feat(ui): flag icon ng

--- src/app/pages/auth/sign-up/index.tsx
+++ src/app/pages/auth/sign-up/index.tsx
@@ -16,6 +16,7 @@ import {
 import { LinkPrimary } from "@/components/link"
 import { Logo } from "@/components/logo"
 import { Button } from "@/components/button"
+import { FlagNg } from "@/assets/icons/flag-ng"
 
 export function SignUp() {
   const [stage, setStage] = useState("details-personal")
@@ -75,20 +76,7 @@ export function SignUp() {
                         type="button"
                         className="absolute right-0 -translate-y-1/2 cursor-pointer rounded-sm bg-neutral-200 px-1.5 py-0.5"
                       >
-                        <svg
-                          className="size-6"
-                          xmlns="http://www.w3.org/2000/svg"
-                          id="flag-icons-ng"
-                          viewBox="0 0 640 480"
-                        >
-                          <g fill-rule="evenodd" stroke-width="1pt">
-                            <path fill="#fff" d="M0 0h640v480H0z" />
-                            <path
-                              fill="#008753"
-                              d="M426.6 0H640v480H426.6zM0 0h213.3v480H0z"
-                            />
-                          </g>
-                        </svg>
+                        <FlagNg className="size-6" />
                       </button>
                     </div>
                   </FieldInput>
