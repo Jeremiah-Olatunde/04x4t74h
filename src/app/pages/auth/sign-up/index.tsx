e19ad53267d8f389d7e45ea560fe7506fa2d74ce
Jesuseun Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2025-07-25 01:39:50 +0100

    feat(ui): page sign-up

--- src/app/pages/auth/sign-up/index.tsx
+++ src/app/pages/auth/sign-up/index.tsx
@@ -28,6 +28,11 @@ export function SignUp() {
 
       <Form>
         <FormField>
+          <FieldLabel text="full name" htmlFor="email" />
+          <FieldInput name="name" type="text" placeholder="Bola Ahmed Tinubu" />
+        </FormField>
+
+        <FormField>
           <FieldLabel text="email address" htmlFor="email" />
           <FieldInput
             name="email"
@@ -37,8 +42,30 @@ export function SignUp() {
         </FormField>
 
         <FormField>
-          <FieldLabel text="full name" htmlFor="email" />
-          <FieldInput name="name" type="text" placeholder="Bola Ahmed Tinubu" />
+          <FieldLabel text="phone number" htmlFor="telephone" />
+          <FieldInput name="telephone" type="tel" placeholder="08178917635">
+            <div className="relative">
+              <button
+                type="button"
+                className="absolute right-0 -translate-y-1/2 cursor-pointer rounded-sm bg-neutral-200 px-1.5 py-0.5"
+              >
+                <svg
+                  className="size-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  id="flag-icons-ng"
+                  viewBox="0 0 640 480"
+                >
+                  <g fill-rule="evenodd" stroke-width="1pt">
+                    <path fill="#fff" d="M0 0h640v480H0z" />
+                    <path
+                      fill="#008753"
+                      d="M426.6 0H640v480H426.6zM0 0h213.3v480H0z"
+                    />
+                  </g>
+                </svg>
+              </button>
+            </div>
+          </FieldInput>
         </FormField>
 
         <FormField>
