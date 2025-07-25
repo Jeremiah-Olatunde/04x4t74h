0034958b9516d0814adac691914675a8d8f3115e
Jesuseun Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2025-07-25 11:06:50 +0100

    chore(setup): class-variance-authority

--- prettier.config.ts
+++ prettier.config.ts
@@ -3,6 +3,8 @@ import { type Config } from "prettier"
 const config: Config = {
   semi: false,
   plugins: ["prettier-plugin-tailwindcss"],
+  tailwindStylesheet: "./src/index.css",
+  tailwindFunctions: ["tw"],
 }
 
 export default config
