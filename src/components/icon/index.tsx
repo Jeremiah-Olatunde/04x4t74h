81a16ff8954cd2494f269a93052957633f37983a
Jesuseun Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2025-07-26 06:29:08 +0100

    feat(ui): component Icon

--- src/components/icon/index.tsx
+++ src/components/icon/index.tsx
@@ -1,16 +1,45 @@
+import { tw } from "@/utils/tailwind"
+import { cva, type VariantProps } from "class-variance-authority"
+
 import { type LucideIcon } from "lucide-react"
 import { AccessibleIcon } from "radix-ui"
+
+const neutral = tw`text-neutral-400`
+const primary = tw`text-primary`
+const secondary = tw`text-secondary`
+const green = tw`text-green-600`
+const red = tw`text-red-600`
+
+const sm = tw`size-4`
+const md = tw`size-5`
+const lg = tw`size-6`
+
+const variants = {
+  variant: { neutral, primary, secondary, green, red },
+  size: { sm, md, lg },
+} as const
+
+const defaultVariants = {
+  variant: "neutral",
+  size: "md",
+} as const
+
+const styles = cva("", { variants, defaultVariants })
 
 export function Icon({
   icon: Icon,
   label,
+  variant,
+  size,
 }: {
   icon: LucideIcon
   label: string
+  variant?: VariantProps<typeof styles>["variant"]
+  size?: VariantProps<typeof styles>["size"]
 }) {
   return (
     <AccessibleIcon.Root label={label}>
-      <Icon className="size-5 text-neutral-400" />
+      <Icon className={styles({ variant, size })} />
     </AccessibleIcon.Root>
   )
 }
