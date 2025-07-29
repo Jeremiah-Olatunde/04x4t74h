b8042cc76dda50f8dca38c8d051e3acb7812ccdb
Jesuseun Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2025-07-29 17:10:14 +0100

    fix: incorrect casing

--- src/assets/icons/flag-ng.tsx
+++ src/assets/icons/flag-ng.tsx
@@ -6,7 +6,7 @@ export function FlagNg(props: React.SVGProps<SVGSVGElement>) {
       id="flag-icons-ng"
       viewBox="0 0 640 480"
     >
-      <g fill-rule="evenodd" stroke-width="1pt">
+      <g fillRule="evenodd" strokeWidth="1pt">
         <path fill="#fff" d="M0 0h640v480H0z" />
         <path fill="#008753" d="M426.6 0H640v480H426.6zM0 0h213.3v480H0z" />
       </g>
