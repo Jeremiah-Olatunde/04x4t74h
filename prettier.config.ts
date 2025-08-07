e37963745546fc4e7d832858b343330363d9f5b4
Jesuseun Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2025-08-07 02:13:09 +0100

    chore(setup): prettier

--- prettier.config.ts
+++ prettier.config.ts
@@ -2,9 +2,9 @@ import { type Config } from "prettier"
 
 const config: Config = {
   semi: false,
-  plugins: ["prettier-plugin-tailwindcss"],
-  tailwindStylesheet: "./src/index.css",
-  tailwindFunctions: ["tw"],
+  // plugins: ["prettier-plugin-tailwindcss"],
+  // tailwindStylesheet: "./src/index.css",
+  // tailwindFunctions: ["tw"],
 }
 
 export default config
