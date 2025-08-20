a34f3d87c44e78c4f1b8fb6079a36fe4afa9bea0
Jesuseun Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2025-08-20 15:32:42 +0100

    feat(ui): component Button

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,23 +1,19 @@
-import type { PropsWithChildren } from "react"
+import type { ComponentProps, PropsWithChildren } from "react"
+
 import { Badge, type BadgeVariantProps } from "@/components/badge"
 import { Pill } from "@/components/pill"
 
-type ButtonProps = {
-  type: "submit" | "button"
-  handleClick: () => void
-}
-
-type ButtonBadgeProps = ButtonProps & BadgeVariantProps
+type ButtonBadgeProps = ComponentProps<"button"> & BadgeVariantProps
 
 export function ButtonBadge({
   children,
   color,
   size,
   type,
-  handleClick,
+  onClick,
 }: PropsWithChildren<ButtonBadgeProps>) {
   return (
-    <button type={type} onClick={handleClick} className="w-full cursor-pointer">
+    <button type={type} onClick={onClick} className="w-full cursor-pointer">
       <Badge color={color} size={size}>
         {children}
       </Badge>
@@ -30,10 +26,10 @@ export function ButtonPill({
   color,
   size,
   type,
-  handleClick,
+  onClick,
 }: PropsWithChildren<ButtonBadgeProps>) {
   return (
-    <button type={type} onClick={handleClick} className="w-full cursor-pointer">
+    <button type={type} onClick={onClick} className="w-full cursor-pointer">
       <Pill color={color} size={size}>
         {children}
       </Pill>
