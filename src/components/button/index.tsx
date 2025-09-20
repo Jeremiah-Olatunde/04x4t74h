5b4ae43ac434bdc013ae68ea050659a2d8b952a1
Jesuseun Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2025-09-20 17:28:53 +0100

    chore: fix type errors

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -13,12 +13,9 @@ import {
   FunnelPlusIcon,
   HeartIcon,
   ListFilterIcon,
-  EyeIcon,
-  EyeOffIcon,
   Share2Icon,
   XIcon,
 } from "lucide-react"
-import { Icon } from "@/components/icon"
 
 type ButtonBadgeProps = ComponentProps<"button"> & BadgeVariantProps
 
