fc3ac824948774407bd51ef64303c877f0d7a216
Jesuseun Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2025-09-21 01:55:09 +0100

    fix(ui): update card

--- src/components/business/card/small/index.tsx
+++ src/components/business/card/small/index.tsx
@@ -12,52 +12,50 @@ export function Small({ business }: SmallProps) {
   })
 
   return (
-    <li>
-      <article className="size-60 relative rounded-xl border border-neutral-100">
-        <LinkWouter href={`/business/${business.id}/home/menu`}>
-          <div className="flex flex-col h-full w-full">
-            <div className="rounded-t-xl grow-1 min-h-0 w-full bg-neutral-50">
-              <img
-                src={business.logo}
-                alt={`cover photo of ${business.name}`}
-                className="rounded-t-xl h-full w-full object-cover block"
-              />
-            </div>
-
-            <div className="p-4 pt-2">
-              <div className="flex flex-row justify-between items-center">
-                <p className="text-xxs font-sora text-neutral-400 font-light">
-                  (N15,000 to unlimited)
-                </p>
-
-                <div className="flex gap-1 items-center">
-                  <StarIcon className="size-3 stroke-secondary fill-secondary" />
-                  <span className="text-xxs font-semibold font-sora">
-                    {formatter.format(business.rating)}
-                  </span>
-                </div>
-              </div>
-
-              <div className="h-1" />
-
-              <header>
-                <h2 className="text-xs font-sora font-medium text-neutral-700 truncate">
-                  {business.name}
-                </h2>
-              </header>
-
-              <div className="h-4" />
-
-              <address className="flex justify-start items-center gap-1">
-                <MapPinIcon className="text-neutral size-3" />
-                <span className="text-xxs font-sora  text-neutral-400 font-light not-italic">
-                  {business.town}, {business.city}
-                </span>
-              </address>
-            </div>
+    <article className="size-60 relative rounded-xl border border-neutral-100">
+      <LinkWouter href={`/business/${business.id}/home/menu`}>
+        <div className="flex flex-col h-full w-full">
+          <div className="rounded-t-xl grow-1 min-h-0 w-full bg-neutral-50">
+            <img
+              src={business.logo}
+              alt={`cover photo of ${business.name}`}
+              className="rounded-t-xl h-full w-full object-cover block"
+            />
           </div>
-        </LinkWouter>
-      </article>
-    </li>
+
+          <div className="p-4 pt-2">
+            <div className="flex flex-row justify-between items-center">
+              <p className="text-xxs font-sora text-neutral-400 font-light">
+                (N15,000 to unlimited)
+              </p>
+
+              <div className="flex gap-1 items-center">
+                <StarIcon className="size-3 stroke-secondary fill-secondary" />
+                <span className="text-xxs font-semibold font-sora">
+                  {formatter.format(business.rating)}
+                </span>
+              </div>
+            </div>
+
+            <div className="h-1" />
+
+            <header>
+              <h2 className="text-xs font-sora font-medium text-neutral-700 truncate">
+                {business.name}
+              </h2>
+            </header>
+
+            <div className="h-4" />
+
+            <address className="flex justify-start items-center gap-1">
+              <MapPinIcon className="text-neutral-400 size-3" />
+              <span className="text-xxs font-sora  text-neutral-400 font-light not-italic">
+                {business.town}, {business.city}
+              </span>
+            </address>
+          </div>
+        </div>
+      </LinkWouter>
+    </article>
   )
 }
